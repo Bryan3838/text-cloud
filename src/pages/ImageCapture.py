fe4d2d0daea292d921fe139a346f309fffc6e477
Maxi-Garcia <71419011+Maxi-Garcia@users.noreply.github.com>
2022-09-20 11:59:55 -0700

    Update ImageCapture.py

--- src/pages/ImageCapture.py
+++ src/pages/ImageCapture.py
@@ -7,10 +7,10 @@ from src.Camera import Camera
 from src.fonts import LARGE_FONT
 
 class ImageCapture(tk.Frame):
-
+    #basically sets up the window for the app with labels for buttons and navigation
     def __init__(self, parent, controller):
         ttk.Frame.__init__(self, parent)
-
+        #Window lable for Image Capture Window
         self.header = ttk.Label(self, text="Image Capture", font=LARGE_FONT)
         self.header.pack(side=tk.TOP)
 
@@ -19,7 +19,7 @@ class ImageCapture(tk.Frame):
 
         self.body = ttk.Frame(self)
         self.body.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
-
+        #Navigation Button for taking an Image using a webcam
         self.button1 = ttk.Button(self, text="Take Image",
             command=lambda: self.take_snapshot(controller))
         self.button1.pack(in_=self.options, side=tk.LEFT, anchor="n")
@@ -31,7 +31,7 @@ class ImageCapture(tk.Frame):
 
         self.camera_process = None
         self.camera = None
-
+    #gets frames from the webcam and stores them into an image array at a speed of 30 ms
     def update(self):
         ret, frame = self.camera.get_frame()
         if ret:
@@ -40,12 +40,12 @@ class ImageCapture(tk.Frame):
             self.image_label.configure(image=image)
             
         self.camera_process = self.image_label.after(30, self.update)
-    
+    #turns on the camera on the starting of the app
     def show(self, controller):
         if not self.camera:
             self.enable_camera()
         controller.show_frame("ImageCapture")
-
+    #takes the capture image of the camera; checks if its in the array and returns the specific frame
     def take_snapshot(self, controller):
         ret, frame = self.camera.get_frame()
         if ret:
@@ -55,11 +55,11 @@ class ImageCapture(tk.Frame):
             page = controller.get_frame("SourceView")
             page.add_data("Image Capture", image, image.text_array)
             page.show(controller)
-
+    #turns on camera
     def enable_camera(self):
         self.camera = Camera()
         self.update()
-
+    #turns off camera after closing window
     def disable_camera(self):
         if self.camera_process:
             self.image_label.after_cancel(self.camera_process)

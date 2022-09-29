9b3ae40d6e7eab8c5f4c0d0b82fa7ce9a5887954
Bryan Tran <bryantran78@gmail.com>
2022-09-29 12:14:17 -0700

    no clean textarray on object creation

--- src/TextArray.py
+++ src/TextArray.py
@@ -7,7 +7,7 @@ nltk.download('stopwords', quiet=True)
 class TextArray():
 
     def __init__(self, text_array):
-        self.text_array = self.cleaned(text_array)
+        self.text_array = text_array
 
     def get_dict(self):
         counted_dict = {i:self.text_array.count(i) for i in self.text_array}
@@ -15,9 +15,9 @@ class TextArray():
         reversed_dict = reversed(sorted_dict)
         return dict(reversed_dict)
 
-    def cleaned(self, text_array):
+    def clean(self, text_array):
         list = [text.lower() for text in text_array] # lowercase elements
         list = [re.sub("[0-9]", '', text) for text in text_array] # filter numbers
         list = [text for text in list if text not in stopwords.words('english')] # filter stopwords
         text = " ".join(list)
-        return re.findall(r"\w+", text)
+        return self.text_array

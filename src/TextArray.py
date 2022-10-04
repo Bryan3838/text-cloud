44380f43708f91dc14744e2a7bd67c8cc799c06e
Bryan Tran <bryantran78@gmail.com>
2022-10-04 11:50:38 -0700

    Update TextArray.py

--- src/TextArray.py
+++ src/TextArray.py
@@ -9,8 +9,8 @@ class TextArray():
     def __init__(self, text_array):
         self.text_array = text_array
 
-    def get_dict(self):
-        counted_dict = {i:self.text_array.count(i) for i in self.text_array}
+    def get_dict(self, text_array):
+        counted_dict = {i:text_array.count(i) for i in text_array}
         sorted_dict = sorted(counted_dict.items(), key=lambda item: item[1])
         reversed_dict = reversed(sorted_dict)
         return dict(reversed_dict)
@@ -19,6 +19,4 @@ class TextArray():
         list = [text.lower() for text in self.text_array] # lowercase elements
         list = [re.sub("[0-9]", '', text) for text in list] # filter numbers
         list = [text for text in list if text not in stopwords.words('english')] # filter stopwords
-        text = " ".join(list)
-        text = re.findall(r"\w+", text)
-        return text
+        return list

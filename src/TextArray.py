92f61b04becfbd3fd14ff456261941a565d73662
Bryan Tran <bryantran78@gmail.com>
2022-10-10 19:24:02 -0700

    comments for presentation

--- src/TextArray.py
+++ src/TextArray.py
@@ -6,10 +6,15 @@ class TextArray():
         self.text_array = text_array
 
 def clean(text_array, stopwords):
-    list = [text.lower() for text in text_array] # lowercase elements
-    list = [re.sub(r"[0-9]", '', text) for text in list] # remove numbers
-    list = [re.sub(r"(?!(?<=[a-z])'[a-z])[^\w\s]", ' ', text, flags=re.I) for text in list] # remove non-puncuated words
-    list = [text.strip() for text in list if text.strip() and (text.strip() not in stopwords)] # remove whitespace, remove empty strings and filter stopwords
+    # lowercase elements
+    list = [text.lower() for text in text_array]
+
+    # remove invalid words and numbers (List: ["hi!,", "their's...", "123"] => ["hi", "their's", ""])
+    list = [re.sub(r"(?!(?<=[a-z])'[a-z])[^\w\s]|[0-9]", ' ', text, flags=re.I) for text in list]
+
+    # remove whitespace, remove empty strings and filter stopwords (List: ["hi", "their's"])
+    list = [text.strip() for text in list if text.strip() and (text.strip() not in stopwords)]
+    
     return list
 
 def get_dict(text_array):
